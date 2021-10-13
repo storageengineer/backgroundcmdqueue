88c72f99477280f59a84c0c0b131abf77bb5489f
storageengineer <36526682+storageengineer@users.noreply.github.com>
2021-10-12 21:18:20 -0500

    Update background.sh
    More details in comments; changed output pipe order to tail/cut/indent

--- background.sh
+++ background.sh
@@ -22,9 +22,9 @@ cmd_clear () {
 
 # add command to queue
 cmd_addqueue () {
-    echo "adding $1"
+    #echo "adding $1"
     cmd_queue+=("${1}")
-    echo "# of elements = ${#cmd_queue[@]}"
+    #echo "# of elements = ${#cmd_queue[@]}"
 }
 
 # list commands
@@ -37,37 +37,53 @@ cmd_printqueue () {
 
 # the guts, this does the magic
 runcommand () {
-    # temp file to "watch" background process
+    # temp file to "watch" the output from the background process
     OutputFile=$(mktemp)
+    
+    # viewport is 6 lines high
+    # pad with (viewport-1) blank lines
     echo -e \n\n\n\n\n > $OutputFile
     
     # exec commands in array
-    echo "runcommand"
-    cmd_printqueue
+    # echo "runcommand"
+    # cmd_printqueue
     
     # run through the queued command in the cmd_queue array
     for value in "${cmd_queue[@]}"; do
     
-        # Execute command, no screen output allowed
-        # Run command, redirect stdout->append to $OutputFile, combine stderr with stdout, run in background 
+        # Execute command
+        # Redirect stdout->append to $OutputFile, combine stderr with stdout, & run in background 
         ${value} >> $OutputFile 2>&1 &
+        # save PID to detect end of task
         PID=$!
         
-        # repeat
+        # repeat (ticker loop)
         while true; do
-            # We have 6 lines to use
-            # Line 1 - ticker
+            # We have viewport lines to use
+            # Line 1: ticker & job command line
             tput bold
             echo -e "\r-   Running ${value}... "
             echo "----------------------------------------"
-            # Line 2-5. column limited tail
+            
+            # Line 2 to (viewport-1). column limited tail
             tput dim
-            cut -c -$(($(tput cols)-4)) $OutputFile | indent | tail -n 5
+            
+            # print last 5 lines of output
+            # cut line width to (screen.width-4)
+            # add 4 spaces to start of line via local function indent
+            # put tail before cut; was other way around
+            # **note** indent adds 4 spaced; the may cause misalignment with tabbed outputs
+            tail -n 5 | cut -c -$(($(tput cols)-4)) $OutputFile | indent
+            
             # if background process isn't running, break
+            # **only way out of the ticker loop**
             sudo kill -0 $PID > /dev/null 2>&1 || break
-            # make ticker TICK
+            
+            # make ticker TICK, i.e. the ticker change rate is about 4 times per second
             sleep 0.25
-            # go back UP 6 lines
+            
+            # go back UP (viewport+1) lines
+            # why +1? 6th line printed with newline
             tput cuu1
             tput cuu1
             tput cuu1
@@ -76,6 +92,9 @@ runcommand () {
             tput cuu1
             tput cuu1
             tput ed
+            
+            # Repeated 2x more times, only differentce is the echo first echo which has the ticker (animated "-")
+            # left in the old comments
             
             # We have 6 lines to use
             # Line 1 - ticker
